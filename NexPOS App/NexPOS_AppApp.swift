f61b8af777ac0e43fffdd3a318c5109b88a7fd2c
Khai Pham <khai.pnp@outlook.com>
2025-02-18 15:40:41 +0700

    Initial Commit

---
+++ NexPOS App/NexPOS_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NexPOS_AppApp.swift
+//  NexPOS App
+//
+//  Created by Khai Pham on 18/2/25.
+//
+
+import SwiftUI
+
+@main
+struct NexPOS_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
